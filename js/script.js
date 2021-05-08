57a0f1e1ef241e757553ea968ed9f615efcd717e
sambitraj <78531354+sambitraj@users.noreply.github.com>
2021-05-08 10:24:17 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* 
-    Summer Hacks
-    SOA Code Room
-    Project Template
-    Use this template: https://github.com/SOACodeRoom/summerhacks-projectName/generate
-    script.js: You will write your Javascript code for your portfolio website here. You can add other Javascript files too but it must be under "js" folder.
-*/
